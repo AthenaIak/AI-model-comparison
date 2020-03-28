028a3f476e2ae787708323167c184713652d62f4
Athena <athena.iak@gmail.com>
2020-03-28 19:05:01 +0200

    CNNMC #10 Move documentation comments for functions inside the function definition

--- comparison_tools.py
+++ comparison_tools.py
@@ -1,16 +1,18 @@
+"""
+The comparison tools are functions that help compare neural networks.
+"""
 from scipy.stats import ks_2samp
 import numpy as np
 
-'''
-Find the Nth layer of the specified type.
-args:
-- model : the model that will be searched
-- type  : the lowercase type, as it is automatically saved by keras in the layer's name (e.g. conv2d, dense)
-- order : 0 by default (the first matching layer will be returned)
-'''
-
 
 def find_layer(model, type, order=0):
+    """
+    Given a model, find the Nth layer of the specified type.
+    :param model: the model that will be searched
+    :param type:  the lowercase type, as it is automatically saved by keras in the layer's name (e.g. conv2d, dense)
+    :param order: 0 by default (the first matching layer will be returned)
+    :return: The index of the matching layer or None if it was not found.
+    """
     num_found = 0
     for layer in model.layers:
         if type + '_' in layer.get_config()['name']:
@@ -22,6 +24,14 @@ def find_layer(model, type, order=0):
 
 
 def get_weights_for_layer(layer):
+    """
+    Extracts the weights and biases of a layer.
+    :param layer: The layer that the weights and biases will be extracted from.
+    :return: A triple consisting of
+             a) the weights,
+             b) the flattened weights (weights as a list of values),
+             c) the biases.
+    """
     layer_weights = layer.get_weights()[0]
     layer_weights_flat = layer_weights.reshape(layer_weights.size)
     layer_bias = layer.get_weights()[1]
@@ -30,11 +40,26 @@ def get_weights_for_layer(layer):
 
 
 def compare_distributions_with_ks(vectorA, vectorB):
+    """
+    Computes the Kolmogorov-Smirnov statistic on 2 vectors.
+    :param vectorA: The first vector we want to compare.
+    :param vectorB: The second vector we want to compare.
+    :return: The Kolmogorov-Smirnov statistic.
+    """
     ks = ks_2samp(vectorA, vectorB)
     return ks
 
 
 def compare_distributions_with_histogram(vectorA, vectorB):
+    """
+    Compare the distributions of two vectors by using a histogram with identical bins.
+    :param vectorA: The first vector we want to compare. The histogram bins will be determined based on this vector.
+    :param vectorB: The second vector we want to compare.
+    :return: A triple consisting of:
+            a) the amount of datapoints the fall into each bin for vectorA
+            b) the amount of datapoints the fall into each bin for vectorB
+            c) the bins
+    """
     hist_a, bins = np.histogram(vectorA)
     hist_b, _ = np.histogram(vectorB, bins)
     return hist_a, hist_b, bins
--- data_manipulator.py
+++ data_manipulator.py
@@ -1,34 +1,35 @@
+"""
+The data manipulator contains functions to change the format of data.
+"""
 import numpy as np
 from keras.utils import to_categorical
 
-'''
-Convert images to 28x28 with decimal values and labels to categorical
-'''
-
 
 def prepare_visual_data(images, labels):
+    """
+    Convert images to 28x28 with decimal values. Convert labels to categorical.
+    :param images: Images provided as a list of vectors (size Nx784) with values 0-255.
+    :param labels: Labels provided as a list of numbers.
+    :return: Reshaped images (shape=28x28 and values=0-1) and labels (converted to a binary class matrix).
+    """
     images = images.reshape((len(images), 28, 28, 1))
     images = images.astype('float32') / 255
     labels = to_categorical(labels)
     return images, labels
 
 
-'''
-Split data to two mutually-exclusive unbalanced sets. 
-The first set is returned (the second can be inferred as returned_val == False)
-args:
-  - labels        : the labels of the whole set that should be split
-  - major_labels  : the label values that the returned set should favour
-  - minor_labels  : the label values that the returned set should not favour
-  - bleed_through : the number of minor elements that bleed through to the returned set 
-                    (expected to be < sum / 2 of each category)
-returns:
-  - a boolean numpy array that can be used to access the labels/data for the first unbalanced set
-'''
-
-
 def split_unbalanced(labels, major_categories, minor_categories, bleed_through):
-    #### Split training data to two unbalanced sets
+    """
+    Split data to two mutually-exclusive unbalanced sets.
+    The first set is returned (the second can be inferred as returned_val == False)
+    :param labels:           the labels of the whole set that should be split
+    :param major_categories: the label values that the returned set should favour
+    :param minor_categories: the label values that the returned set should not favour
+    :param bleed_through:    the number of minor elements that bleed through to the returned set
+                             (expected to be < sum / 2 of each category)
+    :return: a boolean numpy array that can be used to access the labels/data for the first unbalanced set
+    """
+
     # get the indexes per category (0-9)
     all_categories = major_categories + minor_categories
     idx_per_category = [i for i in range(len(all_categories))]
